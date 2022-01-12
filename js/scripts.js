3d032d89775d10c32594a6030a61cd1875917f5e
Jase Seeley <jcseeley31590@yahoo.com>
2022-01-12 13:46:37 -0800

    add subtraction, multiplication and division.
    co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -22,4 +22,28 @@ $(document).ready(function() {
     const result = add(number1, number2);
     $("#output").text(result);
   });
+
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#output1").text(result);
+  });
+
+  $("form#mul").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#mul1").val());
+    const number2 = parseInt($("#mul2").val());
+    const result = multiply(number1, number2);
+    $("#output2").text(result);
+  });
+
+  $("form#div").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#div1").val());
+    const number2 = parseInt($("#div2").val());
+    const result = divide(number1, number2);
+    $("#output3").text(result);
+  });
 });
